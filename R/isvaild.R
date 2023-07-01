870dcec7315c4c14321502c709b1a453a0b36d0a
Chamudi Kashmila <chamudikashmila@Chamudis-MacBook-Pro.local>
2023-07-01 04:05:38 -0400

    added the isValid function

---
+++ R/isvaild.R
@@ -0,0 +1,18 @@
+#rfunctions for find whether the chemicals are valid
+
+isvalid <- function(search_values) {
+
+  utils::data("inventory", envir = environment())
+
+  column_values <- inventory$Abbreviation
+
+
+
+
+  #Check if values in 'search_value'  exist in 'Abbreviatio' column
+  results <- search_values %in% column_values
+
+
+
+  return(results)
+}
